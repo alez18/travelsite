38226b84c30c7c68d1a044eb3dcca648fc1add94
alez18 <alex.lps88@yahoo.com.mx>
2019-05-10 14:23:18 -0500

    10May19
    Update

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(function(){
+	let pinged = false
+	let sticky = document.querySelector("#sticky-navigation")
+	let nav = document.querySelector("#navigation")
+	
+	let stickyScrollPoint = document.querySelector(".h-img").offsetHeight
+
+	function pingToTop(){
+		if(pinged) return;
+		sticky.classList.add("pined")
+		nav.classList.add("hidden")
+		sticky.parentNode.classList.remove("hidden")
+		pinged = true;
+	}
+
+	function unPingFromTop(){
+		if(!pinged) return
+		sticky.classList.remove("pined")
+		nav.classList.remove("hidden")
+		sticky.parentNode.classList.add("hidden")
+		pinged = false
+	}
+
+
+	window.addEventListener("scroll", function(ev){
+		//Se obtiene la altura y distancia, altura, etc.
+		let coords = nav.getBoundingClientRect()
+		if(window.scrollY < stickyScrollPoint) return unPingFromTop()
+		if(coords.top <= 0) return pingToTop()
+		unPingFromTop()
+	})
+
+})()
